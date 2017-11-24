ab0427c591b53615f6f671757605315ced7d7f75
GuillaumeGETTE <guillaume.gette@gmail.com>
2017-11-24 18:09:51 +0100

    printer update

--- Printer.ml
+++ Printer.ml
@@ -9,10 +9,6 @@ let printAnd ="\\land"
 let printImplies ="\\Rightarrow"
 let printTop = "\\top"
 let printBot = "\\bot"
-let printForall = "\\forall"
-let printExists = "\\exists"
-let lbracket = "\\left["
-let rbracket = "\\right]"
 let lpar = "("
 let rpar = ")"
 
@@ -22,27 +18,12 @@ module PRINTER = struct
 
 open SYMBOLS
 
-let rec term_to_string = function
-  |Meta(s, _) -> (("Meta(")^(s^")"))
-  |Variable(s)-> ("Var("^(s^")"))
-  |Constant(s)-> ("Cons("^(s^")"))
-  |Operator(s,l)-> let str = term_list_to_string l in (*if (String.equal str "") then s else*) (s^"(")^(str^")")
-and  _tlts_aux = function
-  |[]->""
-  |t::q->(" , "^(term_to_string t))^(_tlts_aux q)
-and term_list_to_string = function
-     |[]->""
-     |t::q->(term_to_string t)^(_tlts_aux q)
-;;
-
 let rec formula_to_string parenthesis = function
-Predicate(s,l)->let str = term_list_to_string l in if (String.equal str "") then s else (s^"(")^(str^")")
+  |Atom(s)->let str = get_atom_content s
   |Or(f1,f2)->let po = (if parenthesis then lpar else "") in let pf=(if parenthesis then rpar else "") in (po^(formula_to_string true f1))^(((" "^printOr)^" ")^((formula_to_string true f2)^pf))
   |And(f1,f2)->let po = (if parenthesis then lpar else "") in let pf=(if parenthesis then rpar else "") in (po^(formula_to_string true f1))^(((" "^printAnd)^" ")^((formula_to_string true f2)^pf))
   |Implies(f1,f2)->let po = (if parenthesis then lpar else "") in let pf=(if parenthesis then rpar else "") in (po^(formula_to_string true f1))^(((" "^printImplies)^" ")^((formula_to_string true f2)^pf))
   |True-> printTop |False-> printBot
-  |Forall(s,f)->((lbracket^printForall)^(s^", "))^((formula_to_string false f)^rbracket)
-  |Exists(s,f)->((lbracket^printExists)^(s^", "))^((formula_to_string false f)^rbracket)
 ;;
 
 let rec _flts_aux = function
